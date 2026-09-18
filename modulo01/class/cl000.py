7e6c0a7d6e3243819a29d61f328498915cc5c535
Matheus Fragata de Moraes <matheusdemoraes2@gmail.com>
2026-09-18 11:12:26 -0300

    aula de tipagem

---
+++ modulo01/class/cl000.py
@@ -0,0 +1,10 @@
+name = input('Whats your name?')
+age = input('Whats your age?')
+weight = input('Whats your weight?')
+
+def main():
+    print('Hi,', name,'!')
+    print('your age is', age, 'years old')
+    print('and your weight is', weight, 'kilograms')
+
+main()
---
+++ modulo01/class/cl001.py
@@ -0,0 +1,5 @@
+n1 = int(input('Enter a number: '))
+n2 = int(input('Enter another number: '))
+s = n1 + n2
+print('The sum is equal a {}'.format(s))
+print('The sum between {} and {} is {}'.format(n1, n2, s))
