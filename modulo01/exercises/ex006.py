a92e1c769e7889908991618173ef68b216c39170
Matheus Fragata de Moraes <matheusdemoraes2@gmail.com>
2026-09-18 16:13:50 -0300

    Update ex006.py

--- modulo01/exercises/ex006.py
+++ modulo01/exercises/ex006.py
@@ -2,7 +2,7 @@
 
 n1 = int(input('Enter a number: '))
 
-def main(number):
+def main(number: int):
     print('The double is: {}'.format(number*2))
     print('The triple is: {}'.format(number*3))
     print('The square is: {}'.format(number**(1/2)))
