3266f31fb47200a8016fe4b8a3e28f88ff151dbb
Matheus Fragata de Moraes <matheusdemoraes2@gmail.com>
2026-09-22 16:54:13 -0300

    exercicio 36 finalizado

--- modulo02/exercises/ex036.py
+++ modulo02/exercises/ex036.py
@@ -1,1 +1,16 @@
-## Escreva um programa para aprovar o empréstimo bancário para a compra de uma casa. O programa vai perguntar o valor da casa, o salário do comprador e em quantos anos ele vai pagar. Calcule o valor da prestação mensal, sabendo que ela não pode exceder 30% do salário ou então o empréstimo será negato.
+## Escreva um programa para aprovar o empréstimo bancário para a compra de uma casa. O programa vai perguntar o valor da casa, o salário do comprador e em quantos anos ele vai pagar. Calcule o valor da prestação mensal, sabendo que ela não pode exceder 30% do salário ou então o empréstimo será negado.
+
+
+def main():
+
+    house_price = float(input('Qual o valor do imóvel? '))
+    salary = float(input('Qual é o seu salário? '))
+    time = float(input('Irá quitar o imóvel em quantos anos? '))
+
+    prestacao = house_price / (time * 12) ## passando de ano para mês
+
+    status = prestacao < salary
+
+    print(f"Empréstimo liberado com mensalidade de R${house_price / 12} durante {time} anos" if status else f"Empréstimo negado, pois a mensalidade de R${prestacao} excede o salário de R${salary}")
+
+main()
