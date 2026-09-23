63036fc98c0335201e620072cbef86fb4b8394c9
Matheus Fragata de Moraes <matheusdemoraes2@gmail.com>
2026-09-23 16:05:18 -0300

    exercicio 44 completo

--- modulo02/exercises/ex044.py
+++ modulo02/exercises/ex044.py
@@ -3,3 +3,26 @@
 ## à vista no cartão: 5% de desconto
 ## em até 2x no cartão: preço normal
 ## 3x ou mais no cartão: 20% de juros
+
+def main():
+    print(f"========== Fragata's Store ==========")
+    value = float(input("Digite o valor do produto: "))
+
+    pagamento = int(input(f"Formas de pagamento: \n[ 1 ] à vista dinheiro/cheque\n[ 2 ] à vista cartão\n[ 3 ] 2x no cartão\n[ 4 ] 3x ou mais no cartão\nQual é a opção?"))
+
+    if pagamento == 1:
+        payment = value - value * 0.1
+        print(f"desconto de R${value * 0.1:.2f}, total de R${payment:.2f}")
+    elif pagamento == 2:
+        payment = value - value * 0.05
+        print(f"desconto de R${value * 0.05:.2f}, total de R${payment:.2f}")
+    elif pagamento == 3:
+        payment = value
+        print(f"2x de {payment / 2:.2f}, total de R${payment:.2f}")
+    elif pagamento == 4:
+        payment = value + value * 0.2
+        print(f"3x de {payment / 3:.2f}, total de R${payment:.2f}")
+    else:
+        print('Forma de pagamento não encontrada')
+
+main()
